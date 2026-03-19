272b758b860c0635f2779dc2a0eca2aecb653e02
Hithaesh <hithaeshpraveen2002@gmail.com>
2026-03-19 23:20:17 +0530

    #9: values and values_list

--- core/scripts/values_values_list.py
+++ core/scripts/values_values_list.py
@@ -1,6 +1,10 @@
+from datetime import timedelta
+from pprint import pprint
+from django.utils import timezone
+
 from django.db import connection
-from django.db.models.functions import Upper
-from django.db.models import Count, Sum, Prefetch, Avg
+from django.db.models.functions import Concat, Length, Upper
+from django.db.models import CharField, Count, Max, Min, Sum, Prefetch, Avg, Value
 from core.models import *
 
 def run():
@@ -21,7 +25,7 @@ def run():
   """
   2. Transforming or aggregation values in DB level
   """
-  # restaurants = Restaurant.objects.values(name_upper = Upper('name'))
+  # restaurants = Restaurant.objects.values(name_upper=Upper('name'))
   # print(restaurants)
   """
   Output: <QuerySet [{'name_upper': 'PIZZERIA 1'},]>
@@ -104,7 +108,222 @@ def run():
   SQL:[{'sql': 'SELECT COUNT("core_restaurant"."id") AS "id__count" FROM "core_restaurant" WHERE "core_restaurant"."name" LIKE \'c%\' ESCAPE \'\\\'', 'time': '0.001'}]
   """
 
-  ratings_avg = Rating.objects.aggregate(avg=Avg('rating'))
-  print(ratings_avg['avg'])
+  # ratings_avg = Rating.objects.aggregate(avg=Avg('rating'))
+  # print(ratings_avg['avg'])
 
-  print(connection.queries)
+  """
+  Q. Average ratings of each restaurant
+  """
+
+  """
+  Solution: 
+  - use aggregate() when whole queryset result
+  - use annotate() per-object result
+
+  Here it is for each restaurant
+  """
+  ### This is giving the average rating of all the Restaurant model ###
+  # restaurants = Restaurant.objects.aggregate(avg_rating=Avg('ratings_rating'))
+  # print(restaurants)
+
+  """
+  Classic N + 1 query
+
+    # for r in Restaurant.objects.all():
+    #   rating = r.objects.aggregate(avg=Avg('ratings__rating'))
+    #   print(rating.get('avg')) 
+  """
+
+  # restaurants = Restaurant.objects.annotate(avg_rating = Avg('ratings__rating'))
+  # print(restaurants[0])
+
+  """
+  Min() and Max()
+  """
+
+  # income_dict = Sale.objects.aggregate(
+  #   min=Min('income'),
+  #   max=Max('income'),
+  #   avg=Avg('income'),
+  #   count=Count('income')
+  # )
+  # print(income_dict)
+
+  """
+  Last month of the data
+  """
+  # last_month = timezone.now() - timedelta(days=31)
+
+  # print(
+  #   Sale.objects.filter(
+  #   datetime__gte=last_month,
+  #    ).aggregate(
+  #     min=Min('income'),
+  #     max=Max('income'),
+  #     avg=Avg('income'),
+  #     count=Count('income')
+  #   )
+  # )
+
+  """
+  ANNOTATE Models with .annotate method
+  """
+  # Fetch all restaurants, and let's assume we want to get the number of
+  # characters in the name of the restaurant. So "sxy" = 3
+
+  # restaurants = Restaurant.objects.annotate(
+  #   len_name = Length('name')
+  # )
+  # print(restaurants.first().len_name)
+  # print(restaurants.values("name", "len_name"))
+
+  """
+  Q. Filter restaurants name with more than 10 characters
+  """
+  # Using the annotate
+  # restaurants = Restaurant.objects.annotate(
+  #   len_name = Length('name')
+  # ).filter(len_name__gt = 10).values('name', 'len_name')
+
+  # print(restaurants)
+
+
+  """
+  .concat() method used to concatenate two or more fields from the DB
+  """
+  # Restaurant 1 [Rating: 4.3]
+
+  # concatenation = Concat('name', Value(' [Rating: '), Avg('ratings__rating'), Value(']'),
+  #   output_field=CharField()
+  # )
+
+  # restaurants = Restaurant.objects.annotate(
+  #   message=concatenation
+  # )
+
+  # for r in restaurants:
+  #   print(r.message)
+
+  # NOTE: To display the field names that can be accessed from Restaurant model
+  # fields = Restaurant._meta.get_fields()
+
+  # for f in fields:
+  #   print(f.name)
+
+  """
+  Q. Add a total_sales field to all the restaurants
+  """
+  # restaurants = Restaurant.objects.annotate(
+  #   total_sales=Sum('sale__income')
+  # )
+
+  # print([r.total_sales for r in restaurants])
+
+  """
+  SQL: 
+  SELECT r.*, SUM(s.income) as total_sales
+  FROM restaurant as r
+  LEFT OUTER JOIN
+  sale as s 
+  ON r.id = s.restaurant
+  GROUP BY r.id
+
+  Q. Find restaurnats whose total_sales > 1000
+  SELECT r.*, SUM(s.income) as total_sales
+  FROM restaurant as r
+  LEFT JOIN 
+  sale as s
+  ON r.id = s.restaurant
+  GROUP BY r.id
+  HAVING total_sales > 1000
+  """
+  # restaurants = Restaurant.objects.annotate(
+  #   total_sales=Sum('sale__income')
+  # ).filter(total_sales__gt=1000)
+  
+
+  """
+  Q. Annotate a field for number of ratings they recieved
+  """
+
+  # restaurants = Restaurant.objects.annotate(
+  #   no_of_ratings=Count('ratings__rating')
+  # ).values('name', 'no_of_ratings')
+
+  # This is different
+  # print(Restaurant.objects.values('name').annotate(no_of_ratings=Count('ratings')))
+
+  """
+  SQL
+  SELECT r.name, COUNT(ra.rating) as no_of_ratings
+  FROM restaurant r
+  LEFT JOIN
+  rating ra
+  ON r.id = ra.restaurant
+  GROUP BY r.name
+  """
+
+  """
+  Q. Need average rating for all distinct restaurant types
+  """
+  # restaurants = Restaurant.objects.values(
+  #   "restaurant_type"
+  # ).annotate(
+  #   avg_rating = Avg("ratings__rating")
+  # ).distinct()
+
+  """
+  SQL:
+  SELECT r.restaurant_type, AVG(ra.rating)
+  FROM restaurant r
+  LEFT JOIN
+  rating ra
+  ON r.id = ra.restaurant_id
+  GROUP BY r.restaurant_type
+  """
+
+
+  """
+  Q. Total number of sales for each restaurant
+  """
+  restaurant = Restaurant.objects.annotate(total_income = Sum("sale__income")) \
+  .filter(total_sales__gt=1000)
+
+  print(restaurant.values("name", "restaurant_type", "total_income"))
+
+  """
+  SQL:
+  SELECT r.name, r.restaurant_type, SUM(s.income) as total_income
+  FROM restaurant r
+  LEFT JOIN
+  sale s
+  ON r.id = s.restaurant_id
+  GROUP BY r.id, r.name, r.restaurant_type
+  HAVING SUM(s.income) > 1000
+  """
+
+  # NOTE: Before Aggregation use WHERE, After Aggregation use HAVING
+  """
+  In SQL:
+  - Non aggregated fields used in SELECT statements must be added to GROUP BY
+  """
+
+"""
+BELOW IS THE CLASSIC EXAMPLE OF N + 1 Query
+"""
+
+# Executes 1 query to fetch distinct restaurant types
+restaurant_types = Restaurant.objects.values_list('restaurant_type', flat=True).distinct()
+
+# Loop runs N times
+for type in restaurant_types:
+    # Executes 1 query per iteration → N queries
+    restaurants = Restaurant.objects.filter(
+        restaurant_type=type
+    ).annotate(avg_rating=Avg('ratings__rating'))
+
+    for r in restaurants:
+        print(f"{r.restaurant_type} restaurant avg rating: {r.avg_rating}")
+
+# Total queries = 1 + N
+pprint(connection.queries)
